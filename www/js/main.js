866026b1abbc3fe4fa84a65fdb1509eacf022fe6
Andre Christoga <andrechristoga@gmail.com>
2015-12-17 17:06:50 +0700

    Login function

---
+++ www/js/main.js
@@ -0,0 +1,14 @@
+function login() {
+	var nama = document.pesan.nama.value;
+	var no-bpjs = document.pesan.no-bpjs.value;
+	var name = "Andre Christoga";
+	var bpjs-no = "";
+
+	if ((nama == name) && (no-bpjs == bpjs-no)) {
+		window.location.href = "rs.html";
+		return false;
+	};
+	else {
+		alert("Harap mendaftar terlebih dahulu")
+	}
+}
