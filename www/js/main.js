8701fc4075b56ef31d87ef2a99a7c3917b51e5be
Andre Christoga <andrechristoga@gmail.com>
2015-12-18 10:21:17 +0700

    Remove everything from login function

--- www/js/main.js
+++ www/js/main.js
@@ -4,15 +4,5 @@ var action = document.pesan.action = login();
 var rs = ["Royal Progress", "RSUP Dr Cipto Mangunkusumo", "RSUP Fatmawati", "RSUP Persahabatan"]
 
 function login() {
-	var nama = document.pesan.nama.value;
-	var name = "Andre Christoga";	
-
-	if ((name == nama)) {
-		window.location.href = "rs.html";
-		return true;
-	};
-	else {
-		alert("Harap mendaftar terlebih dahulu")
-		return false;
-	}
+	
 }
