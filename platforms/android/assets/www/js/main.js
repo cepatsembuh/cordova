458777bb575da50b494afafae74f129974b6c111
Andre Christoga <andrechristoga@gmail.com>
2015-12-18 09:44:14 +0700

    Prepare for build

--- platforms/android/assets/www/js/main.js
+++ platforms/android/assets/www/js/main.js
@@ -1,5 +1,7 @@
+var cordova = require("cordova");
 window.onload = action
-var action = document.pesan.action = login();
+var action = document.pesan.action = login();	
+var rs = ["Royal Progress", "RSUP Dr Cipto Mangunkusumo", "RSUP Fatmawati", "RSUP Persahabatan"]
 
 function login() {
 	var nama = document.pesan.nama.value;
