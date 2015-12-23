17c0af78dc457c83679f7b1482ffeb68a77e6a14
Andre Christoga <andrechristoga@gmail.com>
2015-12-23 11:48:59 +0700

    Using chart.js for hospital data

--- www/js/main.js
+++ www/js/main.js
@@ -21,3 +21,28 @@ function login() {
 		alert("Please register first")
 	}
 }
+
+var ctx = document.getElementById("royal-chart").getContext("2d");
+var myNewChart = new Chart(ctx).PolarArea(data);
+var myPieChart = new Chart(ctx[0]).Pie(data,options);
+
+var data = [
+    {
+        value: 300,
+        color:"#F7464A",
+        highlight: "#FF5A5E",
+        label: "Red"
+    },
+    {
+        value: 50,
+        color: "#46BFBD",
+        highlight: "#5AD3D1",
+        label: "Green"
+    },
+    {
+        value: 100,
+        color: "#FDB45C",
+        highlight: "#FFC870",
+        label: "Yellow"
+    }
+]
