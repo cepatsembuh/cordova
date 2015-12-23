92d83e6d96c33aced06370baaa0eb82088b4d214
Andre Christoga <andrechristoga@gmail.com>
2015-12-23 15:20:49 +0700

    Remove validate name

--- www/js/main.js
+++ www/js/main.js
@@ -3,25 +3,6 @@ window.onload = action
 var action = document.pesan.action = login();	
 var rs = ["Royal Progress", "RSUP Dr Cipto Mangunkusumo", "RSUP Fatmawati", "RSUP Persahabatan"]
 
-function login() {
-	var nama = document.pesan.nama.value;
-	var valid = false;
-
-	var name = ["admin"];
-	for (var i = 0; i < name.length; i++) {
-		if ((nama == name[i]) {
-			valid = true;
-			break;
-		};
-	};
-
-	if (valid) {
-		window.location.href='rs.html';
-	}; else {
-		alert("Please register first")
-	}
-}
-
 var ctx = document.getElementById("royal-chart").getContext("2d");
 var myNewChart = new Chart(ctx).PolarArea(data);
 var myPieChart = new Chart(ctx[0]).Pie(data,options);
