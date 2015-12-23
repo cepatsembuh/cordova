4d9dfbcf2935e44f880cfdba5b30f4f27115c5f2
Andre Christoga <andrechristoga@gmail.com>
2015-12-23 15:22:05 +0700

    Remove onload

--- www/js/main.js
+++ www/js/main.js
@@ -1,29 +1,2 @@
 var cordova = require("cordova");
-window.onload = action
-var action = document.pesan.action = login();	
 var rs = ["Royal Progress", "RSUP Dr Cipto Mangunkusumo", "RSUP Fatmawati", "RSUP Persahabatan"]
-
-var ctx = document.getElementById("royal-chart").getContext("2d");
-var myNewChart = new Chart(ctx).PolarArea(data);
-var myPieChart = new Chart(ctx[0]).Pie(data,options);
-
-var data = [
-    {
-        value: 300,
-        color:"#F7464A",
-        highlight: "#FF5A5E",
-        label: "Red"
-    },
-    {
-        value: 50,
-        color: "#46BFBD",
-        highlight: "#5AD3D1",
-        label: "Green"
-    },
-    {
-        value: 100,
-        color: "#FDB45C",
-        highlight: "#FFC870",
-        label: "Yellow"
-    }
-]
