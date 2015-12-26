5c3bdb99351575e5e2af5a6d77e84ea3c0bedde9
Andre Christoga <andrechristoga@gmail.com>
2015-12-26 22:18:32 +0700

    Costum notification bar color

--- www/js/main.js
+++ www/js/main.js
@@ -1,6 +1,10 @@
 var cordova = require('cordova')
 var rs = ["Royal Progress", "RSUP Dr Cipto Mangunkusumo", "RSUP Fatmawati", "RSUP Persahabatan"]
 
+if (cordova.platformId == 'android') {
+	StatusBar.backgroundColorByHexString("#009688");
+};
+
 function pesan(){
 
 }
