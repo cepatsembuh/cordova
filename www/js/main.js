6a1f087eccd2b0e6a1f05fa94da4d4d6b8259659
Andre Christoga <andrechristoga@gmail.com>
2015-12-30 18:10:51 +0700

    Validate forms

--- www/js/main.js
+++ www/js/main.js
@@ -4,3 +4,17 @@ var rs = ["Royal Progress", "RSUP Dr Cipto Mangunkusumo", "RSUP Fatmawati", "RSU
 if (cordova.platformId == 'android') {
 	StatusBar.backgroundColorByHexString("#009688");
 };
+
+var valid = true;
+
+// Validate Hospital
+if (document.hospital-form.no-bpjs.value == "") {
+	alert("Mohon mengisi form dengan benar")
+	valid = false;
+};
+
+// Validate Puskesmas
+if (document.puskes-form.no-bpjs.value == "") {
+	alert("Mohon mengisi form dengan benar")
+	valid = false;
+};
