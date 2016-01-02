9c29c43c69fde29772ca0a4e7e89da2a8672358a
Andre Christoga <andrechristoga@gmail.com>
2016-01-02 09:20:59 +0700

    Function valid()

--- www/js/main.js
+++ www/js/main.js
@@ -5,16 +5,18 @@ if (cordova.platformId == 'android') {
 	StatusBar.backgroundColorByHexString("#009688");
 };
 
-var valid = true;
+function valid() {
+	var valid = true;
 
-// Validate Hospital
-if (document.hospital-form.no-bpjs.value == "") {
-	alert("Mohon mengisi form dengan benar")
-	valid = false;
-};
+	// Validate Hospital
+	if (document.hospital-form.no-bpjs.value == "") {
+		alert("Mohon mengisi form dengan benar")
+		valid = false;
+	};
 
-// Validate Puskesmas
-if (document.puskes-form.no-bpjs.value == "") {
-	alert("Mohon mengisi form dengan benar")
-	valid = false;
-};
+	// Validate Puskesmas
+	if (document.puskes-form.no-bpjs.value == "") {
+		alert("Mohon mengisi form dengan benar")
+		valid = false;
+	};
+}
