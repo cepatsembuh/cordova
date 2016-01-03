80abc3a0b610fa4ba2076cd2f9f1e6e6809e9c3f
Andre Christoga <andrechristoga@gmail.com>
2016-01-03 12:21:21 +0700

    Validate function

--- www/js/main.js
+++ www/js/main.js
@@ -1,6 +1,16 @@
 var cordova = require('cordova')
 var rs = ["Royal Progress", "RSUP Dr Cipto Mangunkusumo", "RSUP Fatmawati", "RSUP Persahabatan"]
 
+var input = document.getElementbyId('hospital-form');
+
+function validate() {
+ if input == "" {
+  alert ("Please insert some input")
+ }
+ else {
+  return true;
+}
+
 if (cordova.platformId == 'android') {
 	StatusBar.backgroundColorByHexString("#009688");
 };
