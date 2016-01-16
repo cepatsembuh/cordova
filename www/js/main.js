998a0efcaf16356aac35f74454be82d8c116e779
Andre Christoga <andrechristoga@gmail.com>
2016-01-16 11:20:24 +0700

    Change ' to "

--- www/js/main.js
+++ www/js/main.js
@@ -1,5 +1,5 @@
 function data() {
-	var ref = new Firebase('https://cepatsembuh.firebaseio.com/');
+	var ref = new Firebase("https://cepatsembuh.firebaseio.com/");
 
 	var bpjs-rs = document.getElementbyId("bpjs-rs").value;
 	var bpjs-puskesmas = document.getElementbyId("bpjs-puskesmas").value;
