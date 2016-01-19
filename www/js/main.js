60c9b2204dea7aeb6e7fa5ab200bfff11948b4fa
Andre Christoga <andrechristoga@gmail.com>
2016-01-19 18:16:44 +0700

    Get input value

--- www/js/main.js
+++ www/js/main.js
@@ -2,8 +2,19 @@ function data() {
 	var url = "https://cepatsembuh.firebaseio.com/";
 	var ref = new Firebase(url);
 	
+	// Hospital input
+	var bpjs-rs $('#name-rs').val();
 	var bpjs-rs $('#bpjs-rs').val();
+	var nik-rs $('#nik-rs').val();
+	var sex-rs $('#sex-rs').val();
+	var date-rs $('#date-rs').val();
+
+	// Puskesmas input
+	var name-puskesmas $('#name-puskesmas').val();
 	var bpjs-puskesmas $('#bpjs-puskesmas').val();
+	var nik-puskesmas $('#nik-puskesmas').val();
+	var sex-puskesmas $('#sex-puskesmas').val();
+	var date-puskesmas $('#date-puskesmas').val();
 
 	ref.push(bpjs-rs);
 	ref.push(bpjs-puskesmas);
