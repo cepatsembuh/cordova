1156d17797f1e86a3d36998112b2d8948185dd96
Andre Christoga <andrechristoga@gmail.com>
2016-01-21 20:22:01 +0700

    Underscore variable

--- www/js/main.js
+++ www/js/main.js
@@ -3,43 +3,43 @@ function data() {
 	var ref = new Firebase(url);
 	
 	// Hospital input
-	var rs = document.getElementById('hospital-form').value();
-	var name-rs = document.getElementById('name-rs').value();
-	var bpjs-rs = document.getElementById('bpjs-rs').value();
-	var nik-rs = document.getElementById('nik-rs').value();
-	var sex-rs = document.getElementById('sex-rs').value();
-	var date-rs = document.getElementById('date-rs').value();
+	var rs = document.getElementById('hospital_form').value();
+	var name_rs = document.getElementById('name_rs').value();
+	var bpjs_rs = document.getElementById('bpjs_rs').value();
+	var nik_rs = document.getElementById('nik_rs').value();
+	var sex_rs = document.getElementById('sex_rs').value();
+	var date_rs = document.getElementById('date_rs').value();
 
 	// Puskesmas input
-	var puskesmas = document.getElementById('puskesmas-form').value;
-	var name-puskesmas = document.getElementById('name-puskesmas').value();
-	var bpjs-puskesmas = document.getElementById('bpjs-puskesmas').value();
-	var nik-puskesmas = document.getElementById('nik-puskesmas').value();
-	var sex-puskesmas = document.getElementById('sex-puskesmas').value();
-	var date-puskesmas = document.getElementById('date-puskesmas').value();
+	var puskesmas = document.getElementById('puskesmas_form').value;
+	var name_puskesmas = document.getElementById('name_puskesmas').value();
+	var bpjs_puskesmas = document.getElementById('bpjs_puskesmas').value();
+	var nik_puskesmas = document.getElementById('nik_puskesmas').value();
+	var sex_puskesmas = document.getElementById('sex_puskesmas').value();
+	var date_puskesmas = document.getElementById('date_puskesmas').value();
 
-	ref.push(bpjs-rs);
-	ref.push(bpjs-puskesmas);
+	ref.push(bpjs_rs);
+	ref.push(bpjs_puskesmas);
 
 	var rs = ref.child("rs");
 	rs.set({	  
 	  person: {
-	    nama: name-rs,
-	    bpjs: bpjs-rs,
-	    nik: nik-rs,
-	    sex: sex-rs,
-	    date: date-rs,
+	    nama: name_rs,
+	    bpjs: bpjs_rs,
+	    nik: nik_rs,
+	    sex: sex_rs,
+	    date: date_rs,
 	  },	  
 	});
 	
 	var puskesmas = ref.child("puskesmas");
 	puskesmas.set({	  
 	  person: {
-	    nama: name-puskesmas,
-	    bpjs: bpjs-puskesmas,
-	    nik: nik-puskesmas,
-	    sex: sex-puskesmas,
-	    date: date-puskesmas,
+	    nama: name_puskesmas,
+	    bpjs: bpjs_puskesmas,
+	    nik: nik_puskesmas,
+	    sex: sex_puskesmas,
+	    date: date_puskesmas,
 	  },	  
 	});
 
