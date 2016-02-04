eef70248104319b256aa014539cf554a29c3e209
Andre Christoga <andrechristoga@gmail.com>
2016-02-04 18:39:10 +0700

    Random commit

--- www/js/main.js
+++ www/js/main.js
@@ -1,19 +1,20 @@
-// Hospital form
-$('#rs').on('submit') {
-	if (window.location == 'form-rs.html') {
-		window.location.href = 'state-rs.html';
-	};
-	else {
-		alert("Please fill out the input again");
-	}		
-}
+// // Hospital form
+// $('#rs').on('submit') {
+// 	if (window.location == 'form-rs.html') {
+// 		window.location.href = 'state-rs.html';
+// 	};
+// 	else {
+// 		alert("Please fill out the input again");
+// 	}		
+// }
 
-// Puskesmas form
-$('#puskesmas').on('submit') {
-	if (window.location == 'form-puskesmas.html') {
-		window.location.href = 'state-puskesmas.html';
-	};
-	else {
-		alert("Please fill out the input again");
-	}		
-}
+// // Puskesmas form
+// $('#puskesmas').on('submit') {
+// 	if (window.location == 'form-puskesmas.html') {
+// 		window.location.href='https://mail.google.com/mail/u/0/?view=cm&fs=1&tf=1&source=mailto&to=tobatmailid@gmail.com&body=Nama+%3D+Andre+Christoga%0D%0ANIK+atau+NISN(No+KTP)+%3D+1234%0D%0A++++++++++++++++	'
+// 		window.location.href = 'state-puskesmas.html';
+// 	};
+// 	else {
+// 		alert("Please fill out the input again");
+// 	}		
+// }
