49d1b84d9c91ecf002b9f71081e069c92a32dabd
Andre Christoga <andrechristoga@gmail.com>
2016-02-14 07:10:41 +0700

    Faskes function

--- www/js/main.js
+++ www/js/main.js
@@ -1,2 +1,9 @@
 // Firebase 
 var ref = new Firebase("https://cepatsembuh.firebaseio.com/");
+
+function faskes(nama, bpjs, kecamatan, antrian) {
+  this.nama = nama;
+  this.bpjs = bpjs;
+  this.kecamtan = kecamatan;
+  this.antrian = antrian;
+};
