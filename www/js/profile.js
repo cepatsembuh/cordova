07d0977d99b6a81424ede3daf9ea427a9809d503
Andre Christoga <andrechristoga@gmail.com>
2016-04-02 10:48:47 +0700

    Remove doctor profile alert

--- www/js/profile.js
+++ www/js/profile.js
@@ -7,18 +7,3 @@ $('#not-available').on('click', function() {
 function selectFaskes(username) {
   window.location.href = 'faskes/' + username + '.html'
 }
-
-// Doctor profile using SweetAlert
-function doctorProfile(nama, gambar, lulusan, tahun) {
-  swal({
-    title: "dr. " + nama,
-    imageUrl: gambar,
-    text: "Lulusan: " + lulusan + "<br>" + "Tahun: " + tahun,
-    html: true
-  })
-  // Log input value for testing
-  console.log('Nama: ' + nama);
-  console.log('Gambar: ' + gambar);
-  console.log('Lulusan: ' + lulusan);
-  console.log('Tahun: ' + tahun);
-}
