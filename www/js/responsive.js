c142e1b5c1a041b072ec0758691cf850d31367fe
Andre Christoga <andrechristoga@gmail.com>
2016-04-08 00:11:46 +0700

    Responsive image

---
+++ www/js/responsive.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+  var width = $(window).width();
+  var costumizedWith = width - 25;
+  var faskesImage = $('.faskes');
+  faskesImage.css('width', costumizedWith)
+});
