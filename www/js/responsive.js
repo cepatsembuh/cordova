33b0d99a25b57ece4f7346b8e2c2665e0e9ced6f
Andre Christoga <andrechristoga@gmail.com>
2016-04-08 09:28:09 +0700

    Fix wrong variable names

--- www/js/responsive.js
+++ www/js/responsive.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   var width = $(window).width(),
-      costumizedWith = width - 25,
+      costumizedWidth = width - 25,
       faskesImage = $('.faskes');
-  faskesImage.css('width', costumizedWith)
+  faskesImage.css('width', costumizedWidth)
 });
