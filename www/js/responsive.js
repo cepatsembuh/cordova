c8a4cf18b44cefb20a93af471f0d70d2f796aad5
Andre Christoga <andrechristoga@gmail.com>
2016-04-13 14:38:00 +0700

    Responsive ads

--- www/js/responsive.js
+++ www/js/responsive.js
@@ -2,5 +2,7 @@ $(document).ready(function() {
   var width = $(window).width(),
       costumizedWidth = width - 25,
       faskesImage = $('.faskes');
+      ads = $('.ads')
   faskesImage.css('width', costumizedWidth)
+  ads.css('width', costumizedWidth)
 });
