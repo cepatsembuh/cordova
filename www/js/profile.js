e0139835a39397418d40cba9647e565f6dd634c0
Andre Christoga <andrechristoga@gmail.com>
2016-04-22 17:53:36 +0700

    Remove commented code

--- www/js/profile.js
+++ www/js/profile.js
@@ -13,21 +13,6 @@ $('#not-available').on('click', function() {
 	alert("Faskes is not available")
 })
 
-/* function updateData(tipe, username, name) {
-  var faskesRef = new Firebase("https://cepatsembuh.firebaseio.com/" + tipe + "/faskes/" + username);
-  faskesRef.on("value", function(snapshot) {
-    data = snapshot.val().antrian;
-    plus = 1;
-    glee = data + plus
-    console.log('Updating data.. ');
-    faskesRef.set({
-      antrian: glee,
-      nama: name
-    });
-  })
-} */
-
-
 // Get no antrian function
 function getNoAntri(tipe, username, name) {
   // Define firebase URL
