cfe2139053051bac81e261c4686a4c93e0dc1d03
Andre Christoga <andrechristoga@gmail.com>
2016-04-27 10:22:39 +0700

    Some log

--- www/js/profile.js
+++ www/js/profile.js
@@ -58,6 +58,7 @@ function getNoAntri(tipe, username, name) {
 
 function tempatTidur(tipe, username) {
   brea = new Firebase("https://cepatsembuh.firebaseio.com/" + tipe + '/faskes/' + username);
+  console.log('Getting data..');
   brea.on("value", function(snapshot) {
     data = snapshot.val().tempat_tidur;
     alert('Jumlah Tempat Tidur: ' + data);
