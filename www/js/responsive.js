b0b8bc4ae9e612b3a51e5c30d38dc9ec8724ffac
Andre Christoga <andrechristoga@gmail.com>
2016-04-29 04:32:17 +0700

    Expected semicolon

--- www/js/responsive.js
+++ www/js/responsive.js
@@ -1,8 +1,9 @@
 $(document).ready(function() {
   var width = $(window).width(),
       costumizedWidth = width - 25,
-      faskesImage = $('.faskes');
-      ads = $('.ads')
+      faskesImage = $('.faskes'),
+      ads = $('.ads');
+      
   faskesImage.css('width', costumizedWidth - 5)
   ads.css('width', costumizedWidth)
 });
