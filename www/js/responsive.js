4002e25cc6ab218bad6250fa280f3066129e207b
Andre Christoga <andrechristoga@gmail.com>
2016-04-29 04:35:22 +0700

    Expected semicolongst

--- www/js/responsive.js
+++ www/js/responsive.js
@@ -2,8 +2,10 @@ $(document).ready(function() {
   var width = $(window).width(),
       costumizedWidth = width - 25,
       faskesImage = $('.faskes'),
-      ads = $('.ads');
+      ads = $('.ads'),
+      button = $('.button');
       
   faskesImage.css('width', costumizedWidth - 5)
   ads.css('width', costumizedWidth)
+  button.css('width', 100%)
 });
