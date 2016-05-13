25cc3572fd542f33764cae34dba1255844c34fd8
Andre Christoga <andrechristoga@gmail.com>
2016-05-13 19:39:20 +0700

    I'm coding on a theater right now.

--- www/js/profile.js
+++ www/js/profile.js
@@ -32,11 +32,12 @@ function getNoAntri(tipe, username, name) {
 
         return currentData;
     }, function(error, committed, snapshot) {
+        nomor = Number(snapshot.val());
         if (error) {
             alert('Koneksi anda tidak stabil' + error);
         } else {
             alert('Nomor Antrian: ' + snapshot.val());
-            pasien.push().set({
+            nomor.push().set({
               nama: nama,
               nik: nik,
               no_antri: snapshot.val()
