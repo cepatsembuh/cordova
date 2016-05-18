8260a24c0ab648778c12926331cb3205a8c6bc1f
Andre Christoga <andrechristoga@gmail.com>
2016-05-18 19:14:53 +0700

    Updated platforms code

--- platforms/ios/www/js/main.js
+++ platforms/ios/www/js/main.js
@@ -23,19 +23,20 @@ function selectFaskes(username) {
 // Puskesmas form action
 function puskesmas() {
 	// Firebase
-  var ref = new Firebase("https://cepatsembuh.firebaseio.com/"),
+  var ref = new Firebase("http://cepatsembuh.firebaseio.com/"),
 	    puskesmas = ref.child("puskesmas"),
 	    pasien = puskesmas.child("pasien");
 
 	// Input value
 	var nama = $('#nama').val(),
 	    nik = $('#nik').val(),
-	    lokasi = $('#lokasi').val ();
+	    lokasi = $('#lokasi').val();
 
 	if (nama == '') {
 		// Validate name input
 		alert('Nama is not filled');
 	} else if (nik.length != 16) {
+      // Validate nik input
 	   alert('NIK anda tidak valid');
 	} else {
       // Log input value for testing
@@ -43,25 +44,37 @@ function puskesmas() {
       console.log('NIK: ' + nik);
       console.log('Lokasi: ' + lokasi);
 
-      // Push input value to firebase
-      pasien.push().set({
-        nama: nama,
-        nik: nik,
-       lokasi: lokasi
-     });
      switch (lokasi) {
-       default:
-         window.location.href = 'option' + '/available.html';
+      //  Jakarta Utara
+      case "kelapa_gading":
+         window.location.href = 'option/' + 'kelapa-gading.html';
          break;
+      case "cilincing":
+ 			 		window.location.href = 'option/' + 'cilincing.html';
+ 			 		break;
+ 			case "koja":
+ 					window.location.href = 'option/' + 'koja.html';
+ 					break;
+ 			case "pademangan":
+ 					window.location.href = 'option/' + 'pademangan.html';
+ 					break;
+ 			case "penjaringan":
+ 					window.location.href = 'option/' + 'penjaringan.html';
+ 					break;
+ 			case "tanjung_priok":
+ 					window.location.href = 'option/' + 'priok.html';
+ 					break;
+      // Jakarta Barat
+      case "cengkareng":
+          window.location.href = 'option/' + 'cengkareng.html'
      };
-
-	}
+   }
 };
 
 // RSUD form action
 function rsud() {
 	// Firebase
-  var ref = new Firebase("https://cepatsembuh.firebaseio.com/"),
+  var ref = new Firebase("http://cepatsembuh.firebaseio.com/"),
 	    rsud = ref.child("rsud"),
 	    pasien = rsud.child("pasien");
 
@@ -86,26 +99,18 @@ function rsud() {
       console.log('NIK: ' + nik);
       console.log('Lokasi: ' + lokasi);
 
-      // Push input value to firebase
-      pasien.push().set({
-        nama: nama,
-        no_rujuk: rujuk,
-        nik: nik,
-        lokasi: lokasi
-      });
       switch (lokasi) {
         default:
           window.location.href = 'option' + '/available.html';
           break;
       };
-
-	}
+    }
 };
 
 // RSUK form action
 function rsuk() {
 	// Firebase
-  var ref = new Firebase("https://cepatsembuh.firebaseio.com/"),
+  var ref = new Firebase("http://cepatsembuh.firebaseio.com/"),
 	    rsuk = ref.child("rsuk"),
 	    pasien = rsuk.child("pasien");
 
@@ -130,13 +135,6 @@ function rsuk() {
       console.log('NIK: ' + nik);
       console.log('Lokasi: ' + lokasi);
 
-      // Push input value to firebase
-      pasien.push().set({
-        nama: nama,
-        no_rujuk: rujuk,
-        nik: nik,
-        lokasi: lokasi
-      });
       switch (lokasi) {
         default:
           window.location.href = 'option' + '/available.html';
@@ -148,7 +146,7 @@ function rsuk() {
 // RSS form action
 function rss() {
 	// Firebase
-  var ref = new Firebase("https://cepatsembuh.firebaseio.com/"),
+  var ref = new Firebase("http://cepatsembuh.firebaseio.com/"),
 	    rss = ref.child("rss"),
 	    pasien = rss.child("pasien");
 
@@ -173,13 +171,6 @@ function rss() {
       console.log('NIK: ' + nik);
       console.log('Lokasi: ' + lokasi);
 
-      // Push input value to firebase
-      pasien.push().set({
-        nama: nama,
-        no_rujuk: rujuk,
-        nik: nik,
-        lokasi: lokasi
-      });
       switch (lokasi) {
         default:
           window.location.href = 'option' + '/available.html';
@@ -187,3 +178,118 @@ function rss() {
       };
 	}
 }
+
+/* case "cengkareng":
+    window.location.href = 'option/' + 'cengkareng.html';
+    break;
+case "grogol_petamburan":
+    window.location.href = 'option/' + 'grogol_petamburan.html';
+    break;
+case "kalideres":
+    window.location.href = 'option/' + 'kalideres.html';
+    break;
+case "kebon_jeruk":
+    window.location.href = 'option/' + 'kebon_jeruk.html';
+    break;
+case "kembangan":
+    window.location.href = 'option/' + 'kembangan.html';
+    break;
+case "palmerah":
+    window.location.href = 'option/' + 'palmerah.html';
+    break;
+case "taman_sari":
+    window.location.href = 'option/' + 'taman_sari.html';
+    break;
+case "tambora":
+    window.location.href = 'option/' + 'tambora.html';
+    break;
+case "cilandak":
+    window.location.href = 'option/' + 'cilandak.html';
+    break;
+case "jagakarsa":
+    window.location.href = 'option/' + 'jagakarsa.html';
+    break;
+case "kebayoran_baru":
+    window.location.href = 'option/' + 'kebayoran_baru.html';
+    break;
+case "kebayoran_lama":
+    window.location.href = 'option/' + 'kebayoran_lama.html';
+    break;
+case "mampang_prapatan":
+    window.location.href = 'option/' + 'mampang_prapatan.html';
+    break;
+case "pancoran":
+    window.location.href = 'option/' + 'pancoran.html';
+    break;
+case "pasar_minggu":
+    window.location.href = 'option/' + 'pasar_minggu.html';
+    break;
+case "pesanggrahan":
+    window.location.href = 'option/' + 'pesanggrahan.html';
+    break;
+case "setia_budi":
+    window.location.href = 'option/' + 'setia_budi.html';
+    break;
+case "tebet":
+    window.location.href = 'option/' + 'tebet.html';
+    break;
+case "cakung":
+    window.location.href = 'option/' + 'cakung.html';
+    break;
+case "cipayung":
+    window.location.href = 'option/' + 'cipayung.html';
+    break;
+case "ciracas":
+    window.location.href = 'option/' + 'ciracas.html';
+    break;
+case "duren_sawit":
+    window.location.href = 'option/' + 'duren_sawit.html';
+    break;
+case "jatinegara":
+    window.location.href = 'option/' + 'jatinegara.html';
+    break;
+case "kramat_jati":
+    window.location.href = 'option/' + 'kramat_jati.html';
+    break;
+case "makassar":
+    window.location.href = 'option/' + 'makassar.html';
+    break;
+case "matraman":
+    window.location.href = 'option/' + 'matraman.html';
+    break;
+case "pasar_rebo":
+    window.location.href = 'option/' + 'pasar_rebo.html';
+    break;
+case "pulo_gadung":
+    window.location.href = 'option/' + 'pulo_gadung.html';
+    break;
+case "cempaka_putih":
+    window.location.href = 'option/' + 'cempaka_putih.html';
+    break;
+case "gambir":
+    window.location.href = 'option/' + 'gambir.html';
+    break;
+case "johar_baru":
+    window.location.href = 'option/' + 'johar_baru.html';
+    break;
+case "kemayoran":
+    window.location.href = 'option/' + 'kemayoran.html';
+    break;
+case "menteng":
+    window.location.href = 'option/' + 'menteng.html';
+    break;
+case "sawah_besar":
+    window.location.href = 'option/' + 'sawah_besar.html';
+    break;
+case "senen":
+    window.location.href = 'option/' + 'senen.html';
+    break;
+case "tanah_abang":
+    window.location.href = 'option/' + 'tanah_abang.html';
+    break;
+case "kep_seribu_selatan":
+    window.location.href = 'option/' + 'kep_seribu_selatan.html';
+    break;
+case "kep_seribu_utara":
+    window.location.href = 'option/' + 'kep_seribu_utara.html';
+    break; */
--- platforms/ios/www/js/profile.js
+++ platforms/ios/www/js/profile.js
@@ -3,84 +3,74 @@
 * Copyright 2016 Cepat Sembuh
 */
 
-// Firebase go online whenever the application is open
-document.addEventListener("deviceready", function () {
+document.addEventListener('deviceready', function () {
     Firebase.goOnline();
 });
 
-// Not available faskes
 $('#not-available').on('click', function() {
-	alert("Faskes is not available")
+	alert('Faskes is not available.')
 })
 
-// Get no antrian function
 function getNoAntri(tipe, username, name) {
-  // Define firebase URL
-  var faskesRef = new Firebase("https://cepatsembuh.firebaseio.com/" + tipe + "/faskes/" + username);
+  breanna = new Firebase('http://cepatsembuh.firebaseio.com/' + tipe + '/faskes/' + username),
+  antri = new Firebase('http://cepatsembuh.firebaseio.com/' + tipe + '/faskes/' + username + '/antrian');
+  pasien = breanna.child("pasien"),
+  // Date
+  date = new Date(),
+  year = date.getFullYear(),
+  month = date.getMonth() + 1,
+  day = date.getDate(),
+  right_now = year + '-' + month + '-' + day,
+  today = breanna.child(right_now),
 
-  // Log firebase URL
-  console.log('Url :' + "https://cepatsembuh.firebaseio.com/" + tipe + "/faskes/" + username);
+  get = 'Getting data..';
+  console.log(get);
 
-  // Warn user that this fiture need internet
-	alert('Fitur ini membutuhkan internet untuk mengambil data');
+  var nama = prompt("Nama: "),
+      nik = prompt("NIK: ");
 
-  // Confirmation
-  alert("Mohon konfirmasi ulang");
-  var nama = prompt("Masukan nama"),
-      nik = prompt("Masukan NIK:");
-
-  if (nama != "" || nik.length == 16) {
-      // Firebase
-      var pasien = new Firebase("https://cepatsembuh.firebaseio.com/" + tipe + "/faskes/" + username + '/pasien/');
-
-      // Get data
-      faskesRef.on("value", function(snapshot) {
-        // Declare data variables
-        data = snapshot.val().antrian;
-
-        // Print data
-        alert('No antrian: ' + data);
-
-        // Push data to firebase
-        pasien.push().set({
-          nama: nama,
-          nik: nik,
-          nomor_antrian: data
-        })
-      });
+  if (nama === '' || nik.length != 16 || typeof Number(nik) !== 'number' || Number(nik) == 'NaN') {
+    alert('Input tidak valid');
   } else {
-    // Error message
-    alert("Input anda tidak valid. " + "\n" +"Anda tidak bisa mendapatkan nomor antrian");
+    antri.transaction(function(currentRank) {
+        currentData = currentRank + 1;
+
+        return currentData;
+    }, function(error, committed, snapshot) {
+        if (error) {
+            alert('Koneksi anda tidak stabil' + error);
+        } else {
+            alert('Nomor Antrian: ' + snapshot.val());
+            today.push().set({
+              nama: nama,
+              nik: nik,
+              no_antri: snapshot.val()
+            })
+        }
+    });
   }
+  console.log('Data is ready');
 }
 
 function tempatTidur(tipe, username) {
-  // Firebase
-  brea = new Firebase("https://cepatsembuh.firebaseio.com/" + tipe + '/faskes/' + username + '/tempat_tidur');
+  brea = new Firebase('http://cepatsembuh.firebaseio.com/' + tipe + '/faskes/' + username + '/tempat_tidur');
 
-  // Log
   wait = 'Getting data..';
   console.log(wait);
   alert(wait);
 
-  // Get data
-  brea.on("value", function(snapshot) {
-    // Data's
+  brea.on('value', function(snapshot) {
     satu = snapshot.val().satu;
     dua = snapshot.val().dua;
     tiga = snapshot.val().tiga;
 
-    // Show user the data's
-    alert("Kelas I: " + satu + "\n" + "Kelas II: " + dua + "\n" + "Kelas III: " + tiga);    
+    alert('Kelas I: ' + satu + '\n' + 'Kelas II: ' + dua + '\n' + 'Kelas III: ' + tiga); // Show user the data's
   })
 }
 
-function doctorProfile(nama, gambar, lulusan, tahun) {
-  // Text
-  giant = "Lulusan: " + lulusan + "\n" + "Tahun: " + tahun;
-  name = "dr. " + nama;
-  image = "../../img/" + gambar;
+function doctorProfile(nama, lulusan, tahun) {
+  giant = 'Lulusan: ' + lulusan + '\n' + 'Tahun: ' + tahun;
+  name = 'dr. ' + nama;
 
-  // Pop-up
-  alert(name + "\n" + giant);
+  alert(name + '\n' + giant); // Pop-up
 }
