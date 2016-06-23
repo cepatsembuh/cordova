fc03cdc46402e2c61b2b97c09077b3ccbe4ff99d
Andre Christoga <andrechristoga@gmail.com>
2016-06-23 10:39:25 +0700

    Hour and minutes variable

--- www/js/profile.js
+++ www/js/profile.js
@@ -57,6 +57,8 @@ function pilihPoli(username) {
   day = date.getDate(),
   right_now = year + '-' + month + '-' + day,
   today = dipcifica.child(right_now),
+  jam = dipcifica.child('jam'),
+  menit = dipcifica.child('menit'),
 
   bpu = dipcifica.child('bpu'),
   bpg = dipcifica.child('bpg'),
