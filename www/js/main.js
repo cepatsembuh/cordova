08f116797e34f3edf90dccae8775908c0245c7a5
Andre Christoga <andrechristoga@gmail.com>
2016-08-18 17:41:52 +0700

    I don't know how to commit this

--- www/js/main.js
+++ www/js/main.js
@@ -1,31 +1,75 @@
-var ref = new Firebase('https://cepatsembuh.firebaseio.com'),
-	puskesmas = ref.child('puskesmas'),
-	faskes = puskesmas.child('faskes');
+var ref = new Firebase('https://cepatsembuh.firebaseio.com');
+var puskesmas = ref.child('puskesmas'),
+  	faskes = puskesmas.child('kelapa_gading');
 
 function antrian(){
- var nama = $('#name').val(),
-     nik = $('#nik').val(),
-     no = $('#no').val();
+ var ref = new Firebase('https://cepatsembuh.firebaseio.com'),
+    puskesmas = ref.child('puskesmas'),
+    faskes = puskesmas.child('kelapa_gading'),
+    nama = $('#name').val(),
+    nik = $('#nik').val(),
+    no = $('#no').val(),
+    poli = $('#poli').val();
 
- var antrian = faskes.child('no_antrian');
+ var bpu = faskes.child('bpu'),
+     bpg = faskes.child('bpg'),
+     kia = faskes.child('kia');
  
- antrian.transaction(function(currentRank){
+ if (poli == 'null' || nama == '' || nik.length != 16 || no.length != 20) {
+  alert('Input anda tidak valid, mohon cek lagi')
+ } else if (poli == 'bpu') {
+  bpu.transaction(function(currentRank){
   current = currentRank;
   next = currentRank + 1;
 
-  alert('Nomor Antrian Anda:' + current + '\n' + '\n' + '*Tunjukan nomor antrian ini kepada puskesmas');
- 
- var pasien = faskes.child('pasien');
- pasien.push({
-  nama: nama,
-  nik: nik,
-  no: no
- })
+  return next;
+ }, function(error, snapshot, committed){
+      alert('Nomor Antrian Anda:' + next + '\n' + '\n' + '*Tunjukan nomor antrian ini kepada puskesmas');
+     
+      var pasien = faskes.child('pasien');
+      pasien.push().set({
+       nama: nama,
+       nik: nik,
+       no: no,
+       no_antrian: current,
+       poli: 'bpu'
+      })
+  })
+ } else if (poli == 'bpg'){
+   bpg.transaction(function(currentRank){
+   current = currentRank;
+   next = currentRank + 1;
 
- return next;
- })
-}
+   return next;
+  }, function(error, snapshot, committed){
+       alert('Nomor Antrian Anda:' + next + '\n' + '\n' + '*Tunjukan nomor antrian ini kepada puskesmas');
+      
+       var pasien = faskes.child('pasien');
+       pasien.push().set({
+        nama: nama,
+        nik: nik,
+        no: no,
+        no_antrian: current,
+        poli: 'bpg'
+       })
+   })
+ } else if (poli == 'kia'){
+   kia.transaction(function(currentRank){
+   current = currentRank;
+   next = currentRank + 1;
 
-function open(){
-  window.location.href = 'option.html'
+   return next;
+  }, function(error, snapshot, committed){
+       alert('Nomor Antrian Anda:' + next + '\n' + '\n' + '*Tunjukan nomor antrian ini kepada puskesmas');
+      
+       var pasien = faskes.child('pasien');
+       pasien.push().set({
+        nama: nama,
+        nik: nik,
+        no: no,
+        no_antrian: current,
+        poli: 'kia'
+       })
+   })
+ }
 }
