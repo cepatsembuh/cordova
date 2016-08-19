74b51fc2bc631893ce3c9e41b0e0cd973c47c37d
Andre Christoga <andrechristoga@gmail.com>
2016-08-19 14:13:48 +0700

    Remove splashscreen plugin

--- platforms/android/assets/www/cordova_plugins.js
+++ platforms/android/assets/www/cordova_plugins.js
@@ -1,13 +1,6 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
 module.exports = [
     {
-        "file": "plugins/cordova-plugin-splashscreen/www/splashscreen.js",
-        "id": "cordova-plugin-splashscreen.SplashScreen",
-        "clobbers": [
-            "navigator.splashscreen"
-        ]
-    },
-    {
         "file": "plugins/cordova-plugin-whitelist/whitelist.js",
         "id": "cordova-plugin-whitelist.whitelist",
         "runs": true
@@ -16,7 +9,6 @@ module.exports = [
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "cordova-plugin-splashscreen": "3.2.2-dev",
     "cordova-plugin-whitelist": "1.2.0"
 };
 // BOTTOM OF METADATA
--- platforms/android/assets/www/js/main.js
+++ platforms/android/assets/www/js/main.js
@@ -1,194 +1,85 @@
-/*
-* Cepat Sembuh v1.0
-* Copyright 2016 Cepat Sembuh
-*/
+// Firebase child's
+var ref = new Firebase('https://cepatsembuh.firebaseio.com');
+var puskesmas = ref.child('puskesmas'),
+  	faskes = puskesmas.child('kelapa_gading');
 
-// Firebase go online whenever the application is open
-document.addEventListener('deviceready', function () {
-    Firebase.goOnline();
-});
+function antrian(){
+ var ref = new Firebase('https://cepatsembuh.firebaseio.com'),
+    puskesmas = ref.child('puskesmas'),
+    faskes = puskesmas.child('kelapa_gading'),
 
-// Select the services
-function selectServices(tipe) {
-  console.log('User choose: ' + tipe);
-  window.location.href = tipe + '/index.html';
+  // Input's
+  var nama = $('#name').val(),
+      nik = $('#nik').val(),
+      no = $('#no').val(),
+      poli = $('#poli').val();
+
+ // Poli child's
+ var bpu = faskes.child('bpu'),
+     bpg = faskes.child('bpg'),
+     kia = faskes.child('kia');
+ 
+ if (poli == 'null') {
+  alert('Anda belum memilih poli');
+ } else if (nama == '') {
+  alert('Anda belum memasukan nama anda');
+ } else if (nik.length != 16) {
+  alert('Anda belum memasukan nik');
+ } else if (no_rujuk.length != 20) {
+  alert('Anda belum memasukan nomor BPJS');
+ } else if (poli == 'bpu') {
+  bpu.transaction(function(currentRank){
+    current = currentRank;
+    next = currentRank + 1;
+
+  return next;
+ }, function(error, snapshot, committed){
+      alert('Nomor Antrian Anda:' + next + '\n' + '\n' + '*Tunjukan nomor antrian ini kepada puskesmas'); 
+     
+      var pasien = faskes.child('pasien');
+      pasien.push().set({
+       nama: nama,
+       nik: nik,
+       no: no,
+       no_antrian: next,
+       poli: 'bpu'
+      })
+  })
+ } else if (poli == 'bpg'){
+   bpg.transaction(function(currentRank){
+     current = currentRank;
+     next = currentRank + 1;
+
+     return next;
+  }, function(error, snapshot, committed){
+       alert('Nomor Antrian Anda:' + next + '\n' + '\n' + '*Tunjukan nomor antrian ini kepada puskesmas');
+      
+       var pasien = faskes.child('pasien');
+       pasien.push().set({
+        nama: nama,
+        nik: nik,
+        no: no,
+        no_antrian: next,
+        poli: 'bpg'
+       })
+   })
+ } else if (poli == 'kia'){
+   kia.transaction(function(currentRank){
+     current = currentRank;
+     next = currentRank + 1;
+
+     return next;
+  }, function(error, snapshot, committed){
+       alert('Nomor Antrian Anda:' + next + '\n' + '\n' + '*Tunjukan nomor antrian ini kepada puskesmas');
+      
+       var pasien = faskes.child('pasien');
+       pasien.push().set({
+        nama: nama,
+        nik: nik,
+        no: no,
+        no_antrian: next,
+        poli: 'kia'
+       })
+   })
+ }
 }
-
-// Select faskes function
-function selectFaskes(username) {
-  url = '../faskes/' + username + '.html';
-  window.location.href = url;
-}
-
-// Puskesmas form action
-function puskesmas() {
-	// Firebase
-  var ref = new Firebase('http://cepatsembuh.firebaseio.com/'),
-	    puskesmas = ref.child('puskesmas'),
-	    pasien = puskesmas.child('pasien');
-
-	// Input value
-	var nama = $('#nama').val(),
-	    nik = $('#nik').val(),
-      lokasi = $('#lokasi').val();
-      // empire = nik.slice(-16, -15);
-
-	if (nama == '') {
-		// Validate name input
-		alert('Nama is not filled');
-	} else if (nik.length != 16) {
-      // Validate nik input
-	   alert('NIK anda tidak valid');
-	} else {
-      // Log input value for testing
-      console.log('Nama: ' + nama);
-      console.log('NIK: ' + nik);
-
-    //  switch (Number(empire)) {
-    switch(lokasi){
-      //  Jakarta Utara
-      case "kelapa_gading":
-         window.location.href = 'option' + '/kelapa-gading.html';
-         break;
-      case "cilincing":
- 			 		window.location.href = 'option/' + 'cilincing.html';
- 			 		break;
- 			case "koja":
- 					window.location.href = 'option/' + 'koja.html';
- 					break;
- 			case "pademangan":
- 					window.location.href = 'option/' + 'pademangan.html';
- 					break;
- 			case "penjaringan":
- 					window.location.href = 'option/' + 'penjaringan.html';
- 					break;
- 			case "tanjung_priok":
- 					window.location.href = 'option/' + 'priok.html';
- 					break;
-      // Jakarta Barat
-      case "cengkareng":
-          window.location.href = 'option/' + 'cengkareng.html';
-          break;
-      case "grogol":
-          window.location.href = 'option/' + 'grogol.html';
-          break;
-      case "kalideres":
-          window.location.href = 'option/' + 'kalideres.html';
-          break;
-      case "kebon_jeruk":
-          window.location.href = 'option/' + 'kebonjeruk.html';
-          break;
-      case "kembangan":
-          window.location.href = 'option/' + 'kembangan.html';
-          break;
-      case "palmerah":
-          window.location.href = 'option/' + 'palmerah.html';
-          break;
-      case "taman_sari":
-          window.location.href = 'option/' + 'taman-sari.html';
-          break;
-      case "tambora":
-          window.location.href = 'option/' + 'tambora.html';
-          break;
-      // Jakarta Selatan
-      case 'cilandak':
-          window.location.href = 'option/' + 'cilandak.html';
-          break;
-      case 'jagakarsa':
-          window.location.href = 'option/' + 'jagakarsa.html';
-          break;
-      case 'kebayoran_baru':
-          window.location.href = 'option/' + 'kebayoran-baru.html';
-          break;
-      case 'kebayoran_lama':
-          window.location.href = 'option/' + 'kebayoran-lama.html';
-          break;
-      case 'mampang_prapatan':
-          window.location.href = 'option/' + 'mampang.html';
-          break;
-      case 'pancoran':
-          window.location.href = 'option/' + 'pancoran.html';
-          break;
-      case 'pasar_minggu':
-          window.location.href = 'option/' + 'pasar-minggu.html';
-          break;
-      case 'pesanggrahan':
-          window.location.href = 'option/' + 'pesanggrahan.html';
-          break;
-      case 'setia_budi':
-          window.location.href = 'option/' + 'setiabudi.html';
-          break;
-      case 'tebet':
-          window.location.href = 'option/' + 'tebet.html';
-          break;
-      // Jakarta Timur
-      case 'cakung':
-          window.location.href = 'option/' + 'cakung.html';
-          break;
-      case 'cipayung':
-          window.location.href = 'option/' + 'cipayung.html';
-          break;
-      case 'ciracas':
-          window.location.href = 'option/' + 'ciracas.html';
-          break;
-      case 'duren_sawit':
-        window.location.href = 'option/' + 'duren-sawit.html';
-          break;
-      case 'jatinegara':
-          window.location.href = 'option/' + 'jatinegara.html';
-          break;
-      case 'kramat_jati':
-          window.location.href = 'option/' + 'kramat-jati.html';
-          break;
-      case 'makassar':
-          window.location.href = 'option/' + 'makassar.html';
-          break;
-      case 'matraman':
-          window.location.href = 'option/' + 'matraman.html';
-          break;
-      case 'pasar_rebo':
-          window.location.href = 'option/' + 'pasar-rebo.html';
-          break;
-      case 'pulo_gadung':
-          window.location.href = 'option/' + 'pulo-gadung.html';
-          break;
-      // Jakarta Pusat
-      case 'cempaka_putih':
-          window.location.href = 'option/' + 'cempaka-putih.html';
-          break;
-      case 'gambir':
-          window.location.href = 'option/' + 'gambir.html';
-          break;
-      case 'johar_baru':
-          window.location.href = 'option/' + 'johar-baru.html';
-          break;
-      case 'kemayoran':
-          window.location.href = 'option/' + 'kemayoran.html';
-          break;
-      case 'menteng':
-          window.location.href = 'option/' + 'menteng.html';
-          break;
-      case 'sawah_besar':
-          window.location.href = 'option/' + 'sawah-besar.html';
-          break;
-      case 'senen':
-          window.location.href = 'option/' + 'senen.html';
-          break;
-      case 'tanah_abang':
-          window.location.href = 'option/' + 'tanah-abang.html';
-          break;
-      // Pulau Seribu
-      case 'kep_seribu_selatan':
-          window.location.href = 'option/' + 'kep-seribu-selatan.html';
-          break;
-      case 'kep_seribu_utara':
-          window.location.href = 'option/' + 'kep-seribu-utara.html';
-          break;
-
-      // Global
-      case null:
-          alert('Anda belum memilih faskes')
-          break;
-     };
-   }
-};
