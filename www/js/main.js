14b6478f17b801b17f9c19e3189c53fa1bc22e58
Andre Christoga <andrechristoga@gmail.com>
2016-08-19 15:36:20 +0700

    New data's

--- www/js/main.js
+++ www/js/main.js
@@ -1,12 +1,12 @@
 // Firebase child's
-var ref = new Firebase('https://cepatsembuh.firebaseio.com');
-var puskesmas = ref.child('puskesmas'),
+var ref = new Firebase('https://cepatsembuh.firebaseio.com'),
+    puskesmas = ref.child('puskesmas'),
   	faskes = puskesmas.child('kelapa_gading');
 
 function antrian(){
  var ref = new Firebase('https://cepatsembuh.firebaseio.com'),
-    puskesmas = ref.child('puskesmas'),
-    faskes = puskesmas.child('kelapa_gading'),
+     puskesmas = ref.child('puskesmas'),
+     faskes = puskesmas.child('kelapa_gading');
 
   // Input's
   var nama = $('#name').val(),
