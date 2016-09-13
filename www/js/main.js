b1ba942b049d296887fc700969a3aff69e3217e9
Andre Christoga <andrechristoga@gmail.com>
2016-09-13 22:58:57 +0700

    Other poli

--- www/js/main.js
+++ www/js/main.js
@@ -48,13 +48,55 @@ function antrian() {
 
           console.log('HELL YEAH!')
           alert('Nomor Antrian: ' + snapshot.val())
-          
+
           today.push().set({
             nama: nama,
             nik: nik,
             no: no,
             antrian: snapshot.val(),
             poli: 'bpu'
+          })          
+        }
+    })
+ } else if (poli == 'bpg') {
+    bpg.transaction(function(currentRank) {      
+      return currentRank + 1;
+    }, function(error, commited, snapshot) {
+        if (error) {
+          alert('Error')
+        } else {
+          var today = firebase.database().ref('puskesmas/kelapa_gading/pasien/' + right_now + '/bpg');
+
+          console.log('HELL YEAH!')
+          alert('Nomor Antrian: ' + snapshot.val())
+          
+          today.push().set({
+            nama: nama,
+            nik: nik,
+            no: no,
+            antrian: snapshot.val(),
+            poli: 'bpg'
+          })          
+        }
+    })
+ } else if (poli == 'kia') {
+    kia.transaction(function(currentRank) {      
+      return currentRank + 1;
+    }, function(error, commited, snapshot) {
+        if (error) {
+          alert('Error')
+        } else {
+          var today = firebase.database().ref('puskesmas/kelapa_gading/pasien/' + right_now + '/kia');
+
+          console.log('HELL YEAH!')
+          alert('Nomor Antrian: ' + snapshot.val())
+          
+          today.push().set({
+            nama: nama,
+            nik: nik,
+            no: no,
+            antrian: snapshot.val(),
+            poli: 'kia'
           })          
         }
     })
