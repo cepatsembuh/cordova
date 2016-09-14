bde4402a723e3ef568130efebe75da60cad35ec0
Andre Christoga <andrechristoga@gmail.com>
2016-09-14 18:23:21 +0700

    Teting api using a jquery api

--- www/js/callAPI.js
+++ www/js/callAPI.js
@@ -1,4 +1,16 @@
-var xhr = new XMLHttpRequest()
-xhr.open('GET', 'https://codecademy.com')
-
-console.log(xhr.status)
+(function() {
+  var flickerAPI = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
+  $.getJSON( flickerAPI, {
+    tags: "mount rainier",
+    tagmode: "any",
+    format: "json"
+  })
+    .done(function( data ) {
+      $.each( data.items, function( i, item ) {
+        $( "<img>" ).attr( "src", item.media.m ).appendTo( "#images" );
+        if ( i === 3 ) {
+          return false;
+        }
+      });
+    });
+})();
